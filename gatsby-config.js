b3379a3938c3247b543af24f1a4e62540e04d98d
todmy <tolok.d@gmail.com>
2023-03-18 19:20:13 +0100

    refactor(plugins): change order of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,15 @@ module.exports = {
     "gatsby-plugin-image", 
     "gatsby-plugin-sitemap",
     "gatsby-plugin-zopfli",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         "icon": "src/images/icon.png"
       }
-    }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "images",
