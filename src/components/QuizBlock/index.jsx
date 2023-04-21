76430382eda2d3173b002740cd6c57cf3141f2f5
Catherine <katjatolok3@gmai.com>
2023-04-21 23:58:49 +0200

    translate text in folder quizBlock

--- src/components/QuizBlock/index.jsx
+++ src/components/QuizBlock/index.jsx
@@ -9,29 +9,35 @@ const QuizBlock = () => {
   return (
     <BlockLayout className={styles.wrapper}>
       <div className={styles.informativeBlock}>
-        <h1>Still have doubts whether you need this?</h1>
-        <p>
-          If you think that you already know everything you need, take our
-          investment test.
-        </p>
+        <h1>{intl.formatMessage({ id: "quizBlock.stillHave" })}</h1>
+        <p>{intl.formatMessage({ id: "quizBlock.ifYouThink" })}</p>
       </div>
       <div className={styles.mobileCard}>
-        <h1 className={styles.mobileTittle}>What is diversification?</h1>
+        <h1 className={styles.mobileTittle}>
+          {intl.formatMessage({ id: "quizBlock.whatIsDiversification" })}
+        </h1>
         <div className={styles.cardInteractive}>
           <div className={styles.answersMobile}>
             <p className={styles.answerItem}>
-              <span>1</span> It's the variability of the price on the market
+              <span>1</span>&nbsp;
+              {intl.formatMessage({ id: "quizBlock.theVariability" })}
             </p>
             <p className={"${styles.answerItem} ${styles.midleAnswer}"}>
-              <span>2</span> A price decrease
+              <span>2</span>&nbsp;
+              {intl.formatMessage({ id: "quizBlock.aPriceDecrease" })}
             </p>
             <p className={styles.answerItem}>
-              <span>3</span> Dividing the money between different assets
+              <span>3</span>&nbsp;
+              {intl.formatMessage({ id: "quizBlock.dividingTheMoney" })}
             </p>
           </div>
-          <button className={styles.btnNext}>Next</button>
+          <button className={styles.btnNext}>
+            {intl.formatMessage({ id: "quizBlock.next" })}
+          </button>
         </div>
       </div>
     </BlockLayout>
   );
 };
+
+export default QuizBlock;
