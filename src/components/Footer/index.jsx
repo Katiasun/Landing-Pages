931f882fa01263b45869a4a923d71a27ed35312c
Catherine <katjatolok3@gmai.com>
2023-05-09 17:26:15 +0200

    transferred the variables from the en.json file to the index(footer) for translating the text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,55 +10,70 @@ const Footer = () => {
     <BlockLayout className={styles.wrapper}>
       <div className={styles.bakksyInfo}>
         <div className={styles.logoAndPathFinder}>
-          <img src="./footer-logo.png" alt="red logo" />
+          <img
+            src="./footer-logo.png"
+            alt={intl.formatMessage({ id: "footer.redLogo" })}
+          />
           <div className={styles.pathFinder}>
-            <a href="#">Education</a>
-            <a href="#">Giveaway</a>
+            <a href="#">{intl.formatMessage({ id: "footer.education" })}</a>
+            <a href="#">{intl.formatMessage({ id: "footer.giveaway" })}</a>
             <a href="https://www.notion.so/bakksy/81494ba2bdd143a2b44ca6db9125b720?v=6e95eba989f14d60bcef570211645a05">
-              Roadmap
+              {intl.formatMessage({ id: "footer.roadmap" })}
             </a>
           </div>
         </div>
 
         <div className={styles.socials}>
-          <p>Stay up to date</p>
+          <p>{intl.formatMessage({ id: "footer.stayUp" })}</p>
           <div className={styles.mediaLinks}>
             <a href=" https://www.instagram.com/bakksy_app/">
-              <img src="./instagram-logo.png" alt="instagram" />
+              <img
+                src="./instagram-logo.png"
+                alt={intl.formatMessage({ id: "footer.instagram" })}
+              />
             </a>
             <a href="https://www.linkedin.com/company/bakksy">
-              <img src="./linkedin.png" alt="linkedin" />
+              <img
+                src="./linkedin.png"
+                alt={intl.formatMessage({ id: "footer.linkedin" })}
+              />
             </a>
             <a href=" https://www.facebook.com/get.bakksy">
-              <img src="./facebook.png" alt="facebook" />
+              <img
+                src="./facebook.png"
+                alt={intl.formatMessage({ id: "footer.facebook" })}
+              />
             </a>
             <a href="https://twitter.com/Bakksy_app">
-              <img src="./twitter.png" alt="twitter" />
+              <img
+                src="./twitter.png"
+                alt={intl.formatMessage({ id: "footer.twitter" })}
+              />
             </a>
             <a href="https://bakksy.medium.com/">
-              <img src="./carbon_logo-medium.png" alt="carbon" />
+              <img
+                src="./carbon_logo-medium.png"
+                alt={intl.formatMessage({ id: "footer.carbon" })}
+              />
             </a>
           </div>
         </div>
       </div>
       <div className={styles.bakksyCopyright}>
         <div className={styles.twoParagrapsAndBr}>
-          <p>© Bakksy 2023. All rights reserved.</p>
+          <p>{intl.formatMessage({ id: "footer.bakksy2023" })}</p>
           <br />
-          <p>
-            Bakksy is a trading name of Bakksy Finance OÜ, which is a registered
-            in Estonia by address Tornimäe 3, Tallinn 10145 (No. 16575205).
-          </p>
+          <p>{intl.formatMessage({ id: "footer.bakksyIsTrading" })}</p>
         </div>
         <div className={styles.clearConnect}>
           <a href=" https://www.notion.so/bakksy/Terms-Conditions-b1d08d75489b44e8b108b7d20fa396ca">
-            Terms
+            {intl.formatMessage({ id: "footer.terms" })}
           </a>
           <a href="https://www.notion.so/bakksy/Privacy-Policy-99c5b60ef58c48e093c48a9a86cfaeee">
-            Privacy
+            {intl.formatMessage({ id: "footer.privacy" })}
           </a>
           <a href="https://www.notion.so/bakksy/Cookie-Policy-0a66c17ab9b64d4f8ca221bb3c51fed3">
-            Cookies
+            {intl.formatMessage({ id: "footer.cookies" })}
           </a>
         </div>
       </div>
