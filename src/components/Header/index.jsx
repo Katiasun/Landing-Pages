52060472a16db2a88cdbcc943a09988b1d26f47c
Catherine <katjatolok3@gmai.com>
2023-05-10 18:18:55 +0200

    added a translation of the text for header in file en.json

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,33 +12,38 @@ const Header = () => {
   return (
     <BlockLayout className={styles.wrapper}>
       <div className={styles.logo}>
-        <img src={Logo} alt="Bakksy Logo" />
+        <img src={Logo} alt={intl.formatMessage({ id: "header.bakksyLogo" })} />
       </div>
       <nav>
         <ul className={styles.listHeader}>
           <li className={styles.item}>
-            <a href="#">Roadmap</a>
+            <a href="#">{intl.formatMessage({ id: "header.roadmap" })}</a>
           </li>
           <li className={styles.item}>
-            <a href="#">About Bakksy</a>
+            <a href="#">{intl.formatMessage({ id: "header.aboutBakksy" })}</a>
           </li>
           <li className={styles.item}>
-            <a href="#">Waitlist</a>
+            <a href="#">{intl.formatMessage({ id: "header.waitlist" })}</a>
           </li>
           <li className={styles.item}>
-            <a href="#">FAQ</a>
+            <a href="#">{intl.formatMessage({ id: "header.faq" })}</a>
           </li>
         </ul>
       </nav>
       <div className={styles.frameButtons}>
-        <img src={ArrowDown} alt="Arrow Down Icon" />
+        <img
+          src={ArrowDown}
+          alt={intl.formatMessage({ id: "header.arrowIcon" })}
+        />
         <Button
           className={styles.languageSwitcher}
-          aria-label="Language Switcher"
+          aria-label={intl.formatMessage({ id: "header.languageSwitcher" })}
         >
           EN
         </Button>
-        <Button className={styles.investmentBtn}>Learn Investment</Button>
+        <Button className={styles.investmentBtn}>
+          {intl.formatMessage({ id: "header.learnInvestment" })}
+        </Button>
         <div className={styles.burgerMenu} aria-label="Mobile Menu">
           <span className={(`${styles.line}`, `${styles.line1}`)}></span>
           <span className={(`${styles.line}`, `${styles.line2}`)}></span>
