a8636b07d9304d0f8adbcf84087134715729a3f1
Catherine <katjatolok3@gmai.com>
2023-07-17 15:06:32 +0200

    add element Button to CertificateBlock. And change text to intl.formatMessage({id: certificateBlock. ...})

--- src/components/CertificateBlock/index.jsx
+++ src/components/CertificateBlock/index.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import { useIntl } from "gatsby-plugin-react-intl";
 import BlockLayout from "../BlockLayout";
 import * as styles from "./styles.module.css";
+import Certificate from "../../images/Certificate.png";
+import Button from "../Button";
 
 const CertificateBlock = () => {
   const intl = useIntl();
 
-
   return (
     <BlockLayout className={styles.wrapper}>
       <div className={styles.contentYouGet}>
-        <h1>{intl.formatMessage({ id: "CertificateBlock.whatGet" })} 🤩</h1>
-        <h6>{intl.formatMessage({ id: "CertificateBlock.investors" })}</h6>
-        <p>{intl.formatMessage({ id: "CertificateBlock.weWantLearn" })}</p>
+        <h1>{intl.formatMessage({ id: "certificateBlock.whatGet" })} 🤩</h1>
+        <h6>{intl.formatMessage({ id: "certificateBlock.investors" })}</h6>
+        <p>{intl.formatMessage({ id: "certificateBlock.weWantLearn" })}</p>
         <Button type="primary" className={styles.secretFree}>
-          {intl.formatMessage({ id: "CertificateBlock.whyWeGive" })}
+          {intl.formatMessage({ id: "certificateBlock.whyWeGive" })}
         </Button>
-        <p>{intl.formatMessage({ id: "CertificateBlock.itWillBecome" })}</p>
+        <p>{intl.formatMessage({ id: "certificateBlock.itWillBecome" })}</p>
       </div>
       <div className={styles.certificate}>
         <img
           src={Certificate}
-          alt={intl.formatMessage({ id: "CertificateBlock.certificate" })}
+          alt={intl.formatMessage({ id: "certificateBlock.certificate" })}
         />
       </div>
     </BlockLayout>
   );
-
 };
 
 export default CertificateBlock;
